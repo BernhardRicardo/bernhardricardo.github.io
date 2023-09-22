0710f7d7d8a3735e7e4df0100d73dceccba7a5e8
bernhard <Bernhard.R.Putranto@stud.h-da.de>
2023-09-22 14:33:33 +0200

    Update page layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,21 @@
-import Header from "./components/header"
-import Home from "./components/home"
-import Projects from "./components/projects"
+import About from "./components/about";
+import Contact from "./components/contact";
+import Header from "./components/header";
+import Home from "./components/home";
+import Projects from "./components/projects";
 
 export default function Page() {
   return (
-    <main className='bg-white'>
-        <Header />
-        <section className='min-h-screen px-10'>
+    <main className="bg-white relative">
+      <Header />
+      <section className="min-h-screen w-full flex justify-center px-10">
+        <div className="w-3/5">
           <Home />
+          <About />
           <Projects />
-        </section>
+          <Contact />
+        </div>
+      </section>
     </main>
-  )
+  );
 }
