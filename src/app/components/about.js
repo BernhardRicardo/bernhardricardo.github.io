8541a6398ff960aa5f64359d6779ba09fc678293
bernhard <Bernhard.R.Putranto@stud.h-da.de>
2023-09-22 14:33:27 +0200

    Implement about page

---
+++ src/app/components/about.js
@@ -0,0 +1,22 @@
+export default function About() {
+  return (
+    <section className="min-h-screen flex items-center" id="about">
+      <div className=" pb-24">
+        <h2 className="text-5xl text-teal-600 font-medium">About me</h2>
+        <p className="text-md py-5 leading-8 text-gray-800 text-justify">
+          I am a final-year Computer Science student deeply passionate about
+          technology and innovation. My journey through this field has equipped
+          me with a diverse skill set, including expertise in React, a front-end
+          JavaScript library that enables me to create dynamic and user-friendly
+          web applications. With proficiency in React and other programming
+          languages, I thrive on crafting elegant code and enhancing user
+          experiences. My experiences span algorithm design, software
+          development, and web technologies, and I'm always eager to explore
+          emerging trends and technologies. I welcome new collaborations and
+          opportunities to transform concepts into reality, one line of code at
+          a time.
+        </p>
+      </div>
+    </section>
+  );
+}
