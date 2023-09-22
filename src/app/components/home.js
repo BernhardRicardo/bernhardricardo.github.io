0ee8f146c4f567563cb5841070a8c60467e886fe
bernhard <Bernhard.R.Putranto@stud.h-da.de>
2023-09-22 14:16:22 +0200

    Update text

--- src/app/components/home.js
+++ src/app/components/home.js
@@ -1,22 +1,39 @@
-import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai'
-import Image from "next/image"
-import deved from "../../../public/dev-ed-wave.png"
+import Image from "next/image";
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import deved from "../../../public/dev-ed-wave.png";
 
 export default function Home() {
-    return (
-        <section>
-          <div className="text-center">
-          <h2 className='text-5xl py-2 text-teal-600 font-medium'>Bernhard Ricardo Putranto</h2>
-          <h3 className='text-2xl py-2'>Software Engineer</h3>
-          <p className='text-md py-5 leading-8 text-gray-800'>I'm a software engineer based in Jakarta, Indonesia specializing in building (and occasionally designing) exceptional websites, applications, and everything in between.</p>
-        </div>
-        <div className="text-5xl flex justify-center gap-16 text-gray-600">
-          <a href="https://www.linkedin.com/in/bernhardricardoputranto/" target="_blank" className="text-4xl text-black hover:text-black mx-2"><AiFillLinkedin /></a>
-          <a href="https://google.com" target="_blank" className="text-4xl text-black hover:text-black mx-2"><AiFillGithub /></a>
-        </div>
-        <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
-          <Image src={deved} alt="dev ed" layout='fill' objectFit='cover' />
-        </div>
-        </section>
-    )
+  return (
+    <section className="min-h-screen" id="home">
+      <div className="text-center">
+        <h2 className="text-5xl py-8 text-teal-600 font-medium">
+          Bernhard Ricardo Putranto
+        </h2>
+        <h3 className="text-2xl py-2">Web Developer</h3>
+        <p className="text-md py-5 leading-8 text-gray-800">
+          Hi there! I'm a final-year Computer Science student based in
+          Darmstadt, Germany. I'm passionate about building web applications.
+        </p>
+      </div>
+      <div className="text-5xl flex justify-center gap-16 text-gray-600">
+        <a
+          href="https://www.linkedin.com/in/bernhard-ricardo-putranto/"
+          target="_blank"
+          className="text-4xl text-black hover:text-black mx-2"
+        >
+          <AiFillLinkedin />
+        </a>
+        <a
+          href="https://github.com/BernhardRicardo"
+          target="_blank"
+          className="text-4xl text-black hover:text-black mx-2"
+        >
+          <AiFillGithub />
+        </a>
+      </div>
+      <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-60 h-60 mt-10 overflow-hidden">
+        <Image src={deved} alt="dev ed" layout="fill" objectFit="cover" />
+      </div>
+    </section>
+  );
 }
--- src/app/components/projects.js
+++ src/app/components/projects.js
@@ -1,52 +1,86 @@
-import Image from "next/image"
-import web1 from "../../../public/web1.png"
+import Image from "next/image";
+import web1 from "../../../public/web1.png";
 export default function Projects() {
-    return (
-        <section>
-            <div className="text-center">
-                <h2 className='text-5xl py-2 text-teal-600 font-medium'>Projects</h2>
-                <p className='text-md py-5 leading-8 text-gray-800'>Here are some of my projects that I've been working on.</p>
+  return (
+    <section className="min-h-screen pb-56" id="projects">
+      <div className=" pt-20">
+        <h2 className="text-5xl py-2 text-teal-600 font-medium">Projects</h2>
+        <p className="text-md py-5 leading-8 text-gray-800">
+          Here are some of my projects that I've been working on.
+        </p>
+      </div>
+      <div className="flex flex-col max-w-4xl w-full lg:gap-28 mx-auto gap-5">
+        <div className="bg-white shadow-lg rounded-lg flex lg:flex-row flex-col mx-auto">
+          <div className="w-full h-full min-h-min">
+            <Image
+              src={web1}
+              alt="Warehouse Management System"
+              layout="relative"
+              objectFit="cover"
+            />
+          </div>
+          <div className="lg:w-8/12 flex flex-col justify-between">
+            <div className="p-4">
+              <h1 className="font-bold lg:text-xl text-sm mb-2">
+                Warehouse Management System
+              </h1>
+              <p className="text-gray-700 lg:text-base text-xs">
+                A web application for a warehouse management system. The
+                application allows users to manage products, transactions, and
+                customers.
+              </p>
             </div>
-            <div className="flex flex-col max-w-4xl w-full lg:gap-28 mx-auto gap-5">
-                <div className="bg-white shadow-lg rounded-lg flex lg:flex-row flex-col mx-auto">
-                    <div className="w-full h-full min-h-min">
-                        <Image src={web1} alt="Warehouse Management System" layout='relative' objectFit='cover'/>
-                    </div>
-                    <div className="lg:w-8/12 flex flex-col justify-between">
-                        <div className="p-4">
-                            <h1 className="font-bold lg:text-xl text-sm mb-2">Warehouse Management System</h1>
-                            <p className="text-gray-700 lg:text-base text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste atque ea quis molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam voluptatem veniam, est atque cumque eum delectus sint!</p>
-                        </div>
-                        <div className="">
-                            <div className="flex p-4 border-t justify-end border-gray-300 text-gray-700">
-                                <div className="flex-none inline-flex items-center">
-                                    <a href="#" className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow text-sm">Demo</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+            <div className="">
+              <div className="flex p-4 border-t justify-end border-gray-300 text-gray-700">
+                <div className="flex-none inline-flex items-center">
+                  <a
+                    href="#"
+                    className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow text-sm"
+                  >
+                    Demo
+                  </a>
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
-                <div className="bg-white shadow-lg rounded-lg flex lg:flex-row flex-col mx-auto">
-                    <div className="w-full h-full min-h-min">
-                        <Image src={web1} alt="Warehouse Management System" layout='relative' objectFit='cover'/>
-                    </div>
-                    <div className="lg:w-8/12 flex flex-col justify-between">
-                        <div className="p-4">
-                            <h1 className="font-bold lg:text-xl text-sm mb-2">Warehouse Management System</h1>
-                            <p className="text-gray-700 lg:text-base text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste atque ea quis molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam voluptatem veniam, est atque cumque eum delectus sint!</p>
-                        </div>
-                        <div className="">
-                            <div className="flex p-4 border-t justify-end border-gray-300 text-gray-700">
-                                <div className="flex-none inline-flex items-center">
-                                    <a href="#" className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow text-sm">Demo</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+        <section />
+        <div className="bg-white shadow-lg rounded-lg flex lg:flex-row flex-col mx-auto">
+          <div className="w-full h-full min-h-min">
+            <Image
+              src={web1}
+              alt="Warehouse Management System"
+              layout="relative"
+              objectFit="cover"
+            />
+          </div>
+          <div className="lg:w-8/12 flex flex-col justify-between">
+            <div className="p-4">
+              <h1 className="font-bold lg:text-xl text-sm mb-2">
+                Pizza Service
+              </h1>
+              <p className="text-gray-700 lg:text-base text-xs">
+                A web application for a pizza service. The application allows
+                users to order pizza, track the order, and admin to change the
+                order status.
+              </p>
+            </div>
+            <div className="">
+              <div className="flex p-4 border-t justify-end border-gray-300 text-gray-700">
+                <div className="flex-none inline-flex items-center">
+                  <a
+                    href="#"
+                    className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow text-sm"
+                  >
+                    Demo
+                  </a>
                 </div>
-                
-            </div>	
-        </section>
-    )
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
