afa240a7fd459601cafdc2f4347a4a674976ce06
bernhard <Bernhard.R.Putranto@stud.h-da.de>
2023-10-04 18:18:48 +0200

    Update project configuration

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   env: {
     GETFORM_URL: process.env.GETFORM_URL,
   },
+  experimental: {
+    images: true,
+  },
+  output: {
+    export: true,
+  },
 };
 
 module.exports = nextConfig;
